816bef740eaef3d008872d6aace386b70d20e407
AslanBolurov <aslanbolurov@list.ru>
2023-07-02 15:14:26 +0300

    2 Text theory with annotatedBuilder

--- app/src/main/java/com/sumincourse/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sumincourse/firstcomposeproject/MainActivity.kt
@@ -27,10 +27,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -59,27 +62,18 @@ class MainActivity : ComponentActivity() {
 fun TextTest() {
 
     Text(
-        text = "Hello world!",
-        fontSize = 24.sp,
-        fontWeight = FontWeight.ExtraBold,
-//        fontStyle = FontStyle.Italic,
-
-        fontFamily = FontFamily.Cursive,
-//        fontFamily = FontFamily.Serif,
-//        fontFamily = FontFamily.SansSerif,
-
-        textDecoration = TextDecoration.Underline,
-//        textDecoration = TextDecoration.LineThrough,
-//        textDecoration = TextDecoration.combine(
-//            listOf(
-//                TextDecoration.Underline,
-//                TextDecoration.LineThrough
-//            )
-//        )
-
-
-
-
+        buildAnnotatedString {
+            withStyle(SpanStyle(fontWeight = FontWeight.Bold)){
+                append("Hello")
+            }
+            withStyle(SpanStyle(textDecoration = TextDecoration.Underline)){
+                append(" ")
+            }
+            withStyle(SpanStyle(fontSize = 30.sp, textDecoration = TextDecoration.LineThrough)){
+                append("World")
+                append("!")
+            }
+        }
     )
 
 
