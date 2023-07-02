5bf5a1e37c849bedad26ed6f12a37badc3ffdb58
AslanBolurov <aslanbolurov@list.ru>
2023-07-02 12:15:39 +0300

    TimesTable finish

--- app/src/main/java/com/sumincourse/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sumincourse/firstcomposeproject/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,29 +34,42 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun TimesTable(){
+fun TimesTable() {
 
     Column(
         modifier = Modifier
-            .background(color= Color.Yellow)
-            .padding(all=16.dp)
+//            .background(color = Color.Yellow)
+//            .padding(all = 16.dp)
             .fillMaxSize()
 
 
     ) {
-        for(i in 1 until 10) {
+        for (i in 1 until 10) {
             Row(
-                modifier=Modifier
+                modifier = Modifier
                     .fillMaxWidth()
                     .weight(1f)
             ) {
-                for(j in 1 until 10) {
-                    Text("${i*j}")
+                for (j in 1 until 10) {
+                    val colorBackground =if ((i + j) % 2 == 0){
+                        Color.Yellow
+                    }else{
+                        Color.White
+                    }
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(width = 1.dp, color = Color.DarkGray)
+                            .background(color=colorBackground)
+                    ) {
+                        Text("${i * j}")
+                    }
                 }
             }
         }
     }
-
 
 
 }
