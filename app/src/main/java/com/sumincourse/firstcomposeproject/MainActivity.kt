8b652f6e28767c18d41b33aab47c3a45519230b6
AslanBolurov <aslanbolurov@list.ru>
2023-07-02 13:48:33 +0300

    2 card theory

--- app/src/main/java/com/sumincourse/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sumincourse/firstcomposeproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sumincourse.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -12,6 +13,11 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sumincourse.firstcomposeproject.ui.theme.FirstComposeProjectTheme
@@ -34,41 +41,30 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun TimesTable() {
+fun CardTest() {
 
-    Column(
-        modifier = Modifier
-//            .background(color = Color.Yellow)
-//            .padding(all = 16.dp)
-            .fillMaxSize()
-
-
+    Card(
+//        shape= RoundedCornerShape(8.dp)
+//        shape = RectangleShape
+//        shape= CircleShape
+        /* case 1
+        shape= RoundedCornerShape(4.dp).copy(
+            bottomStart = CornerSize(0.dp),
+            bottomEnd = CornerSize(0.dp),
+        )
+         */
+        //case2
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Green,
+            contentColor = Color.White
+        ),
+        border=BorderStroke(1.dp,Color.Black)
     ) {
-        for (i in 1 until 10) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-            ) {
-                for (j in 1 until 10) {
-                    val colorBackground =if ((i + j) % 2 == 0){
-                        Color.Yellow
-                    }else{
-                        Color.White
-                    }
-                    Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .weight(1f)
-                            .border(width = 1.dp, color = Color.DarkGray)
-                            .background(color=colorBackground)
-                    ) {
-                        Text("${i * j}")
-                    }
-                }
-            }
-        }
+        Text(
+            modifier = Modifier.padding(4.dp),
+            text = "Hello world!"
+        )
     }
 
 
---
+++ app/src/main/java/com/sumincourse/firstcomposeproject/ui/theme/TimesTable.kt
@@ -0,0 +1,60 @@
+package com.sumincourse.firstcomposeproject.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Preview
+@Composable
+fun TimesTable() {
+
+    Column(
+        modifier = Modifier
+//            .background(color = Color.Yellow)
+//            .padding(all = 16.dp)
+            .fillMaxSize()
+
+
+    ) {
+        for (i in 1 until 10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1 until 10) {
+                    val colorBackground =if ((i + j) % 2 == 0){
+                        Color.Yellow
+                    }else{
+                        Color.White
+                    }
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(width = 1.dp, color = Color.DarkGray)
+                            .background(color=colorBackground)
+                    ) {
+                        Text("${i * j}")
+                    }
+                }
+            }
+        }
+    }
+
+
+}
