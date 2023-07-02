1e6b8e23f4e9aa0de8a0f5f766797f67c45fea0c
AslanBolurov <aslanbolurov@list.ru>
2023-07-02 12:04:13 +0300

    modifier

--- app/src/main/java/com/sumincourse/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sumincourse/firstcomposeproject/MainActivity.kt
@@ -3,15 +3,20 @@ package com.sumincourse.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sumincourse.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,9 +32,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TimesTable(){
 
-    Column {
+    Column(
+        modifier = Modifier
+            .background(color= Color.Yellow)
+            .padding(all=16.dp)
+            .fillMaxSize()
+
+
+    ) {
         for(i in 1 until 10) {
-            Row {
+            Row(
+                modifier=Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
                 for(j in 1 until 10) {
                     Text("${i*j}")
                 }
