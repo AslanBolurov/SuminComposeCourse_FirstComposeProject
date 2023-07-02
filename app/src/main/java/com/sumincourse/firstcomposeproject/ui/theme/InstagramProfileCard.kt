d837e055981b49d8ae6038e24c205304ec303604
AslanBolurov <aslanbolurov@list.ru>
2023-07-02 13:23:07 +0300

    2 layout

--- app/src/main/java/com/sumincourse/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/sumincourse/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -3,6 +3,7 @@ package com.sumincourse.firstcomposeproject.ui.theme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,8 +11,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -21,39 +24,43 @@ fun InstagramProfileCard() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .height(500.dp)
             .padding(8.dp)
 
         ,
-        horizontalArrangement = Arrangement.SpaceEvenly
+        horizontalArrangement = Arrangement.SpaceEvenly,
 //        horizontalArrangement = Arrangement.SpaceBetween
 //        horizontalArrangement = Arrangement.SpaceAround
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Box(
             modifier = Modifier
-//            .width(50.dp)
-//            .height(50.dp)
                 .size(50.dp)
                 .background(color = Color.Yellow)
-        ) {
+        )
+
+        TwoBoxes()
+
+        TwoBoxes()
+
+        TwoBoxes()
+        
+
+    }
 
 
-        }
+}
+
+@Composable
+private fun TwoBoxes() {
+    Column(
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+
+    ) {
         Box(
             modifier = Modifier
-//            .width(50.dp)
-//            .height(50.dp)
-                .size(50.dp)
-                .background(color = Color.Green)
-        ) {
-
-
-        }
-        Box(
-            modifier = Modifier
-//            .width(50.dp)
-//            .height(50.dp)
-                .size(50.dp)
+                .size(25.dp)
                 .background(color = Color.Blue)
         ) {
 
@@ -61,16 +68,11 @@ fun InstagramProfileCard() {
         }
         Box(
             modifier = Modifier
-//            .width(50.dp)
-//            .height(50.dp)
-                .size(50.dp)
+                .size(25.dp)
                 .background(color = Color.Red)
         ) {
 
 
         }
-
     }
-
-
 }
