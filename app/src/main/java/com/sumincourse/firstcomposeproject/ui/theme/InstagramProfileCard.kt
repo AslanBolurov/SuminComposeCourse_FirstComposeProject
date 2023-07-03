fa38ff7c75b2ace7c5456efd38a2d19de9b1df3e
AslanBolurov <aslanbolurov@list.ru>
2023-07-03 10:22:02 +0300

    2 modifier

--- app/src/main/java/com/sumincourse/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/sumincourse/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -18,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
@@ -58,7 +60,11 @@ fun InstagramProfileCard() {
             Image(
                 painterResource(R.drawable.ic_instagram),
                 contentDescription = "image",
-                modifier = Modifier.size(50.dp)
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .background(Color.White)
+                    .padding(8.dp)
             )
 
             UserStatistics("Posts", "6,950")
